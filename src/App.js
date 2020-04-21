c8ff2cbd01c764484381ecc24f41db61c35e0f5d
Railson Luna <railson.maximino@invillia.com>
2020-04-20 21:00:46 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+
+    });
+  }, []);
   async function handleAddRepository() {
-    // TODO
+   const response = await api.post('repositories', {
+      title: "title one 1",
+      techs: ["php", "java"],
+      url: "www.google.com"
+    })
+    
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    api.delete(`repositories/${id}`).then((response) => {
+      const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+      repositories.splice(repositoryIndex, 1);
+      setRepositories([...repositories]);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repository => 
+            <li key={repository.id}>
+              {repository.title}
+              <button key={repository.id} onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+          </li>
+          )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
